c5008a91c4a8ecf4ca44f62329e874589cbc808c
paulbeck <p.beck@campus.tu-berlin.de>
2016-05-26 07:48:38 +0200

    User added to Database

--- basics/views.py
+++ basics/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest
 from django.template import loader
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.models import User
 # Create your views here.
 from .models import Users
 """
@@ -45,15 +46,46 @@ def logout(request):
     -output: Main- or Indexpage
 """
 def register(request):
+    """
+    email_value = ""
+    password_value = ""
+    toggle_button = False;
 
+        TODO: Get Email/Password and ToggleButton from request.POST['key']
+
+        if request.POST['toggle_button'] = "true"
+            toggle_button = true
+
+    if 'email' in request.POST and 'password' in request.POST:
+        email_value = request.POST['email']
+        password_value = request.POST['password']
+        if request.POST['toggle_button'] == "True":
+            toggle_button = True;
+
+        user = Users(email=email_value, password="password")
+        #   TODO: Add new User Model to Database
+        user.save()
+        #   TODO: Send User Mail
+
+        return redirect('basics:login')
+
+
+    #   TODO: if something wents wrong:
+    return redirect('basics:index')
+
+    """
     if request.method == 'POST':
-        email = request.POST.get('email',None)
-        password = request.POST.get('password',None)
-        check_password = request.POST.get('check_password',None)
-        country = request.POST.get('country',None)
+        if 'email' and 'password' and 'check_password'  in request.POST :
+            email = request.POST.get('email',None)
+            password = request.POST.get('password',None)
+            check_password = request.POST.get('check_password',None)
+            country = request.POST.get('country',None)
+            if password == check_password:
+                #   TODO: with Djangos Users we need a username
+                user = User.objects.create_user(username=email, password=password, email="email ")
+                return login(request)
 
     return render(request, 'basics/actofgoods_startpage.html', {})
->>>>>>> 1c0ab6ce3d4e5b8c498739ee237efb86e5988cdb
 
 
 """
