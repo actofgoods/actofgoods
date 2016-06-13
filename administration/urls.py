7c7adcab0270a71f9aff88dc01fc2d5ca34763a6
paulbeck <p.beck@campus.tu-berlin.de>
2016-06-13 19:52:46 +0200

    added works_on to admin; it is realy done wrong and done like this the side will not be safe.

--- administration/urls.py
+++ administration/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
     url(r'^administration/groups/(?P<pk>\d+)/delete/$', views.group_delete, name='group_delete'),
     url(r'^administration/users/(?P<pk>\d+)/make_admin/$', views.make_admin, name='make_admin'),
     url(r'^administration/categories/(?P<pk>\d+)/delete/$', views.categories_delete, name='categories_delete'),
+    url(r'^administration/work_on_request/$', views.work_on_request, name='work_on_request'),
 ]
