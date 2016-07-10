63bae55e7b9404a6b7338e3bd81fa1b5fe2cfaeb
Thomas D.-S <hanniball1998@gmail.com>
2016-07-10 02:27:18 +0200

    mabye fix for ws_add

--- basics/consumers.py
+++ basics/consumers.py
@@ -14,7 +14,7 @@ import time
 
 @channel_session
 def ws_add(message, room):
-    print(str(message["query_string"]))
+    print("".join(message["query_string"]))
     query = urllib.parse.parse_qs(message['query_string'])
     print(query)
     if 'username' not in query:
