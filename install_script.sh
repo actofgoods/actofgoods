0cfa763b3c306ee951aedd399aec748226491b8a
Thomas D.-S <hanniball1998@gmail.com>
2016-07-10 21:52:31 +0200

    added nginx

--- install_script.sh
+++ install_script.sh
@@ -16,3 +16,11 @@ virtualenv ./venv
 . ./venv/bin/activate
 # install software through pip in virtualenv
 pip3 install -r ./requirements.txt
+# virtualenv deactivate
+deactivate
+basename "$PWD"
+# copy conf file to nginx
+cp ./conf/actofgoods_nginx /etc/nginx/sites-available/actofgoods
+# create sys link for sites-availbe
+ln -s /etc/nginx/sites-available/actofgoods /etc/nginx/sites-enabled
+
