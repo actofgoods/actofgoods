4f71b27f0806d10a317b3afc2a578b5e109d116b
Thomas D.-S <hanniball1998@gmail.com>
2016-07-10 19:56:07 +0200

    added test install_script

---
+++ install_script.sh
@@ -0,0 +1,11 @@
+
+# ------------------------------------------------------------
+# Setup Environment
+# ------------------------------------------------------------
+#!/bin/bash
+message=test
+echo "$message"
+apt-get install postgresql
+apt-get install python3
+apt-get install python3-pip
+apt-get install redis-server
