32f9015d5a34b4c72456eafcbe8888f038d21bd8
Thomas D.-S <hanniball1998@gmail.com>
2016-07-10 20:39:02 +0200

    added virtualenv and pip install

--- install_script.sh
+++ install_script.sh
@@ -5,4 +5,13 @@
 #!/bin/bash
 message=test
 echo "$message"
-apt-get install postgresql python3 python3-pip redis-server
+# install necessery programms through apt-get
+apt-get install postgresql python3 python3-pip redis-server nginx 
+# install virtualenv through pip
+pip3 install virtualenv
+# create virtualenv
+virtualenv ./venv
+#
+. ./venv/bin/activate
+# install software through pip in virtualenv
+pip3 install -r requirments.txt
