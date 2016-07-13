d892adea02f63563d50d9097aee774124326af0a
Thomas D.-S <hanniball1998@gmail.com>
2016-07-13 21:13:33 +0200

    changed

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR="$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )"
 
-source $DIR/venv/bin/activate
-python3 $DIR/manage.py runserver
+source "$DIR"/venv/bin/activate
+python3 "$DIR"/manage.py runserver
